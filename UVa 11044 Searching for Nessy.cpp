cbd377730bd1957b75de0dcd1f4a24f71ef708e6
Khushi369 <94082058+Khushi369@users.noreply.github.com>
2023-03-02 20:44:05 +0530

    Create UVa 11044 Searching for Nessy.cpp

---
+++ UVa 11044 Searching for Nessy.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n;
+    cin>>n;
+    while(n--){
+        int p,q;
+        cin>>p>>q;
+        cout<<(p/3)*(q/3)<<endl;
+    }
+}
